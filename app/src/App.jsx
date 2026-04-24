a45e3bec942628a8e2f6587b90d42eb29aeef720
corevirus-ops <corevirus1738@gmail.com>
2026-04-23 18:35:17 -0700

    added ability to create new todo items

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -25,7 +25,7 @@ async function getTodos() {
      <div className="bg-blue-950 h-screen w-screen flex justify-center items-center">
         <div className="flex flex-col gap-3 bg-gray-50 p-5 rounded-md shadow-gray-500 shadow-2xl min-w-2xl items-center">
           <h1 className="text-gray-600 text-6xl">My Todo List</h1>
-              <TodoForm />
+              <TodoForm setTodoItems={setTodoItems} />
               <TodoItems todoItems={todoItems}/>
         </div>
      </div>
--- app/src/components/TodoForm.jsx
+++ app/src/components/TodoForm.jsx
@@ -1,13 +1,25 @@
 import { useState } from "react";
-import {IoMdAddCircleOutline} from 'react-icons/io'
-export default function TodoForm() {
+import {IoMdAddCircleOutline} from 'react-icons/io';
+import axios from "axios";
+export default function TodoForm({setTodoItems}) {
     const [newTodo, setNewToDo] = useState('');
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+        try {
+            const newItem = await axios.post('http://localhost:5000/todos', {description: newTodo, completed: false});
+            setNewToDo('');
+            setTodoItems((prev) => [...prev, newItem.data]);
+        } catch (err) {
+            console.error(err);
+        }
+    }
 
     return (
         <div className="flex pt-2">
-        <form className="flex p-2 border-2 border-gray-300 rounded-xl shadow-xs shadow-gray-400 justify-evenly">
+        <form className="flex p-2 border-2 border-gray-300 rounded-xl shadow-xs shadow-gray-400 justify-evenly" onSubmit={handleSubmit}>
             <input className="flex focus-within:outline-none" type="text" value={newTodo} onChange={(e) => setNewToDo(e.target.value)} placeholder="What do you plan to do?" />
-            <button className="cursor-pointer hover:text-blue-500 text-2xl"><IoMdAddCircleOutline/></button>
+            <button type="submit" className="cursor-pointer hover:text-blue-500 text-2xl"><IoMdAddCircleOutline/></button>
         </form>
         </div>
     )
