01eaf074f2ea6cd686b7e21683d0b816443c5c7b
corevirus-ops <corevirus1738@gmail.com>
2026-04-23 18:15:46 -0700

    some stylign and introducing the get stage of getting todo items

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,16 +1,32 @@
 import TodoForm from "./components/TodoForm";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [todoItems, setTodoItems] = useState([]);
+
+async function getTodos() {
+  try {
+    const todos = await axios.get('http://localhost:5000/todos');
+    setTodoItems(todos.data);
+        alert(JSON.stringify(todos.data));
+
+  } catch (e) {
+    console.error(e);
+  }
+}
+  
+  useEffect(() => {
+    getTodos();
+  }, []);
 
 
   return (
     <>
      <div className="bg-blue-950 h-screen w-screen flex justify-center items-center">
-        <div className="flex flex-col gap-1.5 bg-gray-50 p-5 rounded-md shadow-gray-500 shadow-2xl min-w-3xl min-h-4/12">
-          <h1 className="text-gray-600 text-6xl p-2">My Todo List</h1>
-            <div className="flex">
+        <div className="flex flex-col gap-3 bg-gray-50 p-5 rounded-md shadow-gray-500 shadow-2xl min-w-2xl items-center">
+          <h1 className="text-gray-600 text-6xl">My Todo List</h1>
               <TodoForm />
-            </div>
         </div>
      </div>
     </>
--- app/src/components/TodoForm.jsx
+++ app/src/components/TodoForm.jsx
@@ -1,12 +1,15 @@
 import { useState } from "react";
+import {IoMdAddCircleOutline} from 'react-icons/io'
 export default function TodoForm() {
     const [newTodo, setNewToDo] = useState('');
 
     return (
-        <form>
-            <input type="text" value={newTodo} onChange={(e) => setNewToDo(e.target.value)} placeholder="What do you plan to do?" />
-
+        <div className="flex pt-2">
+        <form className="flex p-2 border-2 border-gray-300 rounded-xl shadow-xs shadow-gray-400 justify-evenly">
+            <input className="flex focus-within:outline-none" type="text" value={newTodo} onChange={(e) => setNewToDo(e.target.value)} placeholder="What do you plan to do?" />
+            <button className="cursor-pointer hover:text-blue-500 text-2xl"><IoMdAddCircleOutline/></button>
         </form>
+        </div>
     )
 
 }
