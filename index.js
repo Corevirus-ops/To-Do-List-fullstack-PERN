f449fc4a92e217aebe85534c7895810efe2192fe
corevirus-ops <corevirus1738@gmail.com>
2026-05-03 20:08:22 -0700

    added loggin ability, password storing with hashing, with some error handling capability

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const passport = require('passport');
+const session = require('express-session');
+
+
+//routes
 const todosRouter = require('./routes/todos.js');
+const authRouter = require("./routes/auth.js");
 
 dotenv.config();
 
@@ -12,7 +18,17 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use(session({
+  secret: 'comichi wassup',
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.authenticate('session'));
+
 app.use('/todos', todosRouter);
+
+app.use('/', authRouter);
 
 app.listen(port, () => {
 console.log(`listening on port: ${port}`)
---
+++ routes/auth.js
@@ -0,0 +1,74 @@
+
+const express = require("express");
+const router = express.Router();
+const db = require('../db/tododb');
+const passport = require('passport');
+const LocalStrategy = require("passport-local");
+const bcrypt = require("bcryptjs");
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+passport.use(new LocalStrategy(function verify(username, password, cb) {
+  db.query('SELECT * FROM users WHERE email = $1', [username], async function(err, user) {
+    console.log(err);
+    if (err) {return cb(err)}
+    if (!user.rows[0]) {return cb(null, false, { message: 'Incorrect username or password.' });}
+    const match = await bcrypt.compare(password, user.rows[0].password);
+       if (!match) {return cb(null, false, { message: 'Incorrect username or password.' });}
+    user.rows[0].password = null;
+     return cb(null, user.rows[0])
+  });
+  
+}));
+
+passport.serializeUser(function(user, cb) {
+    process.nextTick(function() {
+    cb(null, { id: user.id, username: user.name });
+  });
+});
+
+passport.deserializeUser(function(user, cb) {
+  process.nextTick(function() {
+    return cb(null, user);
+  });
+});
+
+
+router.post('/login/password', passport.authenticate('local', {
+   successRedirect: `${process.env.NETWORK}/${process.env.CLIENT_PORT}/`,
+   failureRedirect: `${process.env.NETWORK}/${process.env.CLIENT_PORT}/login`
+}));
+
+router.post('/create-account', async (req, res) => {
+  const {name, email, password } = req.body;
+  if (!name || !email || !password) {return res.json({err: true, msg: "All Fields Are Required!!"})}
+ 
+  const checkUser = await db.query('SELECT email FROM users WHERE email = $1', [email]);
+  if (checkUser.rowCount > 0) {return res.json({err: true, msg: "User With This Email Already Exists"})}
+
+  const salt = await bcrypt.genSalt(10);
+  const hashPassword = await bcrypt.hash(password, salt);
+
+  const newUser = await db.query('INSERT INTO users (name, email, password) VALUES($1, $2, $3) RETURNING *', [name, email, hashPassword]);
+
+if (!newUser.rows[0]) {return res.status(500).json({err: true, msg: "Something Went Wrong"})}
+
+newUser.rows[0].password = null;
+    req.logIn(newUser.rows[0], function(err) {
+        if (err) { throw err }
+        res.redirect(`${process.env.NETWORK}/${process.env.CLIENT_PORT}/`);
+    });
+
+});
+
+
+router.delete('/logout', function(req, res, next) {
+  req.logout(function(err) {
+    if (err) { return next(err); }
+    res.redirect(`${process.env.NETWORK}/${process.env.CLIENT_PORT}/login`);
+  });
+});
+
+
+module.exports = router;
