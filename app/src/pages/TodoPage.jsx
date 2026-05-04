a2a04691415c50f7038ad9186c1f168fd3d48327
corevirus-ops <corevirus1738@gmail.com>
2026-05-03 17:00:05 -0700

    moving things in prepartion to router and error changes

---
+++ app/src/pages/TodoPage.jsx
@@ -0,0 +1,34 @@
+import TodoForm from "../components/todoPage/TodoForm";
+import TodoItems from "../components/todoPage/TodoItems";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+export default function TodoPage() {
+
+      const [todoItems, setTodoItems] = useState([]);
+
+async function getTodos() {
+  try {
+    const todos = await axios.get(`http://${import.meta.env.VITE_NETWORK}/todos`);
+    setTodoItems(todos.data);
+  } catch (e) {
+    console.error(e);
+  }
+}
+  
+  useEffect(() => {
+    getTodos();
+  }, []);
+
+  return (
+    <>
+            <div className="flex flex-col gap-3 bg-gray-50 rounded-md shadow-gray-500 shadow-2xl items-center sm:h-screen sm:w-screen md:text-2xl md:h-4/5 md:w-4/5 m-auto">
+          <h1 className="text-gray-600 text-6xl p-2">My Todo List</h1>
+              <TodoForm setTodoItems={setTodoItems} />
+              <TodoItems todoItems={todoItems} setTodoItems={setTodoItems}/>
+        </div>
+    </>
+  )
+
+
+}
